83cfcf42456881ce4bc8873603b8733cb697dc70
Dustin Quam <quamsta@gmail.com>
2017-08-09 10:30:43 -0500

    committing config php file

---
+++ mysite/_config.php
@@ -0,0 +1,24 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'lilpicard';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the site locale
+i18n::set_locale('en_US');
+FulltextSearchable::enable();
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+if(Director::isLive()) {
+	Director::forceSSL();
+}
+
+Authenticator::set_default_authenticator('SAMLAuthenticator');
